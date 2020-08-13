a63ea556d3baaa3c0effbe1503ebf9b7a3292503
v_llliiliu <v_llliiliu@tencent.com>
2020-08-13 09:20:32 +0800

    修改本地分支

--- backEnd/config/config.default.js
+++ backEnd/config/config.default.js
@@ -33,9 +33,9 @@ module.exports = appInfo => {
       // 用户名
       user: 'root',
       // 密码
-      password: 'password',
+      password: 'root',
       // 数据库名
-      database: 'myblog',
+      database: 'blog',
     },
     // 是否加载到 app 上，默认开启
     app: true,
